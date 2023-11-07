b913943c5cd87f8ba9fe54b717803b9b79e33a7c
Nguyễn Trần Trung Hậu <ntthau0963@gmail.com>
2023-11-07 22:16:26 +0700

    modify readme

--- src/routes/users/router.tsx
+++ src/routes/users/router.tsx
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as elements from "typed-html";
 import { admin, dentist, patient, staff } from "../auth/router";
+// vd import file gì đó trong layout (Dentist) trong đây, m send dô cái file đó dô layout để code giao diện cho nó
 
 const usersRouter = Router();
 
@@ -14,6 +15,7 @@ usersRouter.get("/patient", patient, async (req, res) => {
 
 usersRouter.get("/dentist", dentist, async (req, res) => {
   return res.send(<p>Dentist</p>);
+  // hoặc return res.send(<Dentist/>);
 });
 
 usersRouter.get("/staff", staff, async (req, res) => {
